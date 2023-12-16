1b6e18d33025a66abaacd5b0b28ccb78bade474e
Raj Anand <raj1809anand@gmail.com>
2023-12-16 07:28:00 +0530

    decorator pattern

---
+++ src/main/java/org/example/Expresso.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Expresso extends Beverage{
+    public Expresso(){
+        description="Expresso";
+    }
+    public double cost() {
+        return 1.99;
+    }
+}
